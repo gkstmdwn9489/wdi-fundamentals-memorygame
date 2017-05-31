300c03b640b291278f2e703ebc2b60b9e1720fcf
Seung Ju Han <seungjuhan@Seungs-MacBook-Air.local>
2017-05-31 15:37:45 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,35 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamons",
+        cardImage: "images/king-of-diamonds.png"
+    }
+
+];
 var cardsInPlay = [];
 
 
 
 function flipCard(cardId){
-    console.log("User flipped "+ cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped "+ cards[cardId].rank);
+    console.log(cards[cardId].suit);
+    console.log(cards[cardId].cardImage);
+    cardsInPlay.push(cards[cardId].rank);
     checkForMatch();
 }
 
