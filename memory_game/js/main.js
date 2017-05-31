ae0371978bdf9f6caf8826f9bbbcce26edf93d13
Seung Ju Han <seungjuhan@Seungs-MacBook-Air.local>
2017-05-31 14:41:17 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,17 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var card = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = card[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+var cardTwo = card[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
 
-console.log("User flipped "+cardOne);
-console.log("User flipped "+cardThree);
+if(cardsInPlay.length === 2){
+    if(cardsInPlay[0] === cardsInPlay[1]){
+        alert("You found a match!");
+    }
+    else{
+        alert("Sorry, try again.");
+    }
+}
